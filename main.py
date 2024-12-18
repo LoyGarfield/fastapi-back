eb77116f7ddc485d32e45b6293cbb80ebf80fc80
LoyGarfield <taukezhanzhunusov@gmail.com>
2024-12-18 09:23:06 +0500

    feat: create a new route

--- main.py
+++ main.py
@@ -9,6 +9,11 @@ app = FastAPI()
 def read_root():
     return {"Hello": "World"}
 
+@app.get("/test")
+def read_root():
+    return {"test": "test"}
+
+
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
